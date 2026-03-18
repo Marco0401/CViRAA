a02233f36cf5cf684f31f2c445660e93d24e14a9
Marco0401 <karmarcc14@gmail.com>
2026-03-18 13:10:39 +0800

    App v1.1

--- api/config.php
+++ api/config.php
@@ -4,6 +4,9 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type');
 
+// Set timezone to Philippines
+date_default_timezone_set('Asia/Manila');
+
 // Database configuration
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
